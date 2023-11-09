4ea3b9c045b3f0346eef59d69f2ea868d57aeb29
Mark Glasman <glasman.mark@gmail.com>
2023-11-09 10:31:45 -0500

    added puppy data to state tracker

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const main = document.querySelector('main')
 const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/2310-fsa-et-web-ft-sf/players`
+const state = {
+  allPuppies: []
+}
+
 async function pullAPIAllPlayers() {
   const response = await fetch(APIURL)
   const puppies = await response.json();
-  console.log(puppies)
+  state.allPuppies = puppies.data.players
+  console.log(state.allPuppies)
 }
 pullAPIAllPlayers()
