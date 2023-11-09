ac396b980bacd2da2d10278744a795dcab119d61
Mark Glasman <glasman.mark@gmail.com>
2023-11-09 10:58:03 -0500

    wrote out render function

--- index.js
+++ index.js
@@ -9,5 +9,25 @@ async function pullAPIAllPlayers() {
   const puppies = await response.json();
   state.allPuppies = puppies.data.players
   console.log(state.allPuppies)
+  renderAllPuppies()
+}
+function renderAllPuppies() {
+  main.innerHTML = ''
+  state.allPuppies.forEach((puppy) => {
+    console.log(puppy.name)
+    const puppyDiv = document.createElement('div')
+    puppyDiv.style.borderTop = "3px solid #000000";
+    puppyDiv.style.borderBottom = "3px solid #000000";
+    html = `
+    <div id=${puppy.id}>
+    <h2>This puppy's name is ${puppy.name} </h2>
+    <img src=${puppy.imageUrl} alt="a cute puppy" style="width: 300px; height: 300px;">
+    </div>
+    `
+    puppyDiv.innerHTML = html
+    main.appendChild(puppyDiv)
+  }
+
+  )
 }
 pullAPIAllPlayers()
