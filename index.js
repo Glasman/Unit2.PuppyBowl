8bdd6b800169e64056bae3e3b0f79d3cfa397eb2
Mark Glasman <glasman.mark@gmail.com>
2023-11-09 10:28:40 -0500

    successfully pulled API

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('yo')
+const main = document.querySelector('main')
+const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/2310-fsa-et-web-ft-sf/players`
+async function pullAPIAllPlayers() {
+  const response = await fetch(APIURL)
+  const puppies = await response.json();
+  console.log(puppies)
+}
+pullAPIAllPlayers()
